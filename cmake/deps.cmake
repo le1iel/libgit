fac04f519010f1410f6ae68de0f2a2cff8678773
le1iel <conform2187@proton.me>
2025-10-05 01:02:31 +0100

    Fixing fetch content builds

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,50 +1,55 @@
 include(FetchContent)
 
-find_package(libgit2)
+find_package(libgit2 QUIET)
 if (libgit2_FOUND)
-# so this is a bit of gymnastics but libgit2 doesn't export a target
-# for the static library, but the brew package does actually have it
-add_library(libgit2::libgit2_static STATIC IMPORTED)
+    # so this is a bit of gymnastics but libgit2 doesn't export a target
+    # for the static library, however the brew package does actually have it
+    add_library(libgit2::libgit2_static STATIC IMPORTED)
 
-# get the location of the shared library
-get_target_property(LIBGIT2_SHARED_LOCATION libgit2::libgit2package LOCATION)
-# get the path where the shared library is
-cmake_path(GET LIBGIT2_SHARED_LOCATION PARENT_PATH LIBGIT2_LIB_PATH)
+    # get the location of the shared library
+    get_target_property(LIBGIT2_SHARED_LOCATION libgit2::libgit2package LOCATION)
 
-# the static library is next to the shared one
-set(LIBGIT2_STATIC_LOCATION "${LIBGIT2_LIB_PATH}/libgit2.a")
+    # get the path where the shared library is
+    cmake_path(GET LIBGIT2_SHARED_LOCATION PARENT_PATH LIBGIT2_LIB_PATH)
 
-# set the properties on the new target
-set_target_properties(libgit2::libgit2_static PROPERTIES
-  IMPORTED_LOCATION "${LIBGIT2_STATIC_LOCATION}"
-  INTERFACE_INCLUDE_DIRECTORIES "$<TARGET_PROPERTY:libgit2::libgit2package,INTERFACE_INCLUDE_DIRECTORIES>"
-  INTERFACE_LINK_LIBRARIES "$<TARGET_PROPERTY:libgit2::libgit2package,INTERFACE_LINK_LIBRARIES>"
-)
+    # the static library is next to the shared one
+    set(LIBGIT2_STATIC_LOCATION "${LIBGIT2_LIB_PATH}/libgit2.a")
+
+    # set the properties on the new target
+    set_target_properties(libgit2::libgit2_static PROPERTIES
+        IMPORTED_LOCATION "${LIBGIT2_STATIC_LOCATION}"
+        INTERFACE_INCLUDE_DIRECTORIES "$<TARGET_PROPERTY:libgit2::libgit2package,INTERFACE_INCLUDE_DIRECTORIES>"
+        INTERFACE_LINK_LIBRARIES "$<TARGET_PROPERTY:libgit2::libgit2package,INTERFACE_LINK_LIBRARIES>"
+    )
 
 else()
-  # I don't even know what is going on with the FetchContent version of this
-  # library
 
-  message(FATAL_ERROR "Fetching library not currently supported")
+    function (cache var value type)
+        set(${var} ${value} CACHE ${type} "" FORCE)
+    endfunction()
 
-  function (cache var value type)
-    set(${var} ${value} CACHE ${type} "" FORCE)
-  endfunction()
+    FetchContent_Declare(libgit2
+        GIT_REPOSITORY
+            "https://github.com/libgit2/libgit2.git"
+        GIT_TAG
+            "v1.9.1"
+        EXCLUDE_FROM_ALL
+    )
 
-  FetchContent_Declare(
-      libgit2
-    GIT_REPOSITORY
-      "https://github.com/libgit2/libgit2.git"
-    GIT_TAG
-      "v1.9.1"
-    EXCLUDE_FROM_ALL
-  )
+    cache(BUILD_SHARED_LIBS OFF BOOL)
+    cache(BUILD_TESTS OFF BOOL)
+    cache(BUILD_CLI OFF BOOL)
+    cache(BUILD_FUZZER OFF BOOL)
 
-  cache(BUILD_SHARED_LIBS OFF BOOL)
-  cache(BUILD_TESTS OFF BOOL)
-  cache(BUILD_CLI OFF BOOL)
+    FetchContent_MakeAvailable(libgit2)
 
-  FetchContent_MakeAvailable(libgit2)
+    target_include_directories(libgit2package
+        INTERFACE 
+            $<BUILD_INTERFACE:${libgit2_SOURCE_DIR}/include>
+    )
+
+    add_library(libgit2::libgit2_static ALIAS libgit2package)
+
 endif()
 
 if(GITXX_BUILD_TESTING)
