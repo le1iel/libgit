63171f90dbd78152f58ee92bea52615a43a8d3a3
le1iel <conform2187@proton.me>
2025-10-08 07:47:20 +0100

    More converters

--- src/gitxx/status.cpp
+++ src/gitxx/status.cpp
@@ -1,26 +1,17 @@
+#include <git2/diff.h>
 #include <git2/errors.h>
 #include <git2/status.h>
 
+#include <algorithm>
 #include <flagfield.hpp>
 #include <gitxx/diff_delta.hpp>
 #include <gitxx/repository.hpp>
 #include <gitxx/status.hpp>
 #include <gitxx/status_options.hpp>
 #include <ranges>
-#include <algorithm>
-#include <git2/diff.h>
+#include <converters.hpp>
 
 namespace {
-
-/// @brief Converts internal options struct to libgit2s struct
-git_status_options convertOptions(const gitxx::StatusOptions &options) {
-  return {.version = 1U,
-          .show = static_cast<git_status_show_t>(options.show),
-          .flags = options.flags.value(),
-          .pathspec = {nullptr, 0U},
-          .baseline = nullptr,
-          .rename_threshold = options.rename_threshold};
-}
 
 gitxx::DiffFile DiffFileFromGit2(const git_diff_file &file) {
   return gitxx::DiffFile{
@@ -55,7 +46,9 @@ template <typename Allocator>
 Status::Status(const BasicRepository<Allocator> *repo,
                const StatusOptions &options, int *res) {
   git_status_list *status_list = nullptr;
-  git_status_options opts = convertOptions(options);
+  git_status_options opts =
+      internal::conversion_traits<git_status_options,
+                                  gitxx::StatusOptions>::from_cpp(options);
 
   auto resOut = git_status_list_new(&status_list, repo->m_repo.get(), &opts);
 
@@ -75,38 +68,38 @@ StatusEntry Status::file(std::string_view file) const noexcept {
   }
 
   auto entries =
-      std::views::iota(size_t{0},
-                       git_status_list_entrycount(m_statusList.get()))
-      | // for each index perform this transform
+      std::views::iota(
+          size_t{0},
+          git_status_list_entrycount(
+              m_statusList.get())) |  // for each index perform this transform
       std::views::transform([this](size_t index) {
         return git_status_byindex(m_statusList.get(), index);
       })
       // for each entry filter our null entries
-      | std::views::filter([](const git_status_entry *entry)
-            { return entry != nullptr; });
+      | std::views::filter(
+            [](const git_status_entry *entry) { return entry != nullptr; });
 
-        auto entry_it = std::ranges::find_if(entries, [&file](const git_status_entry* entry){
-            const git_diff_delta* delta = nullptr;
-            if(entry->head_to_index != nullptr)
-            {
-                delta = entry->head_to_index;
-            }
-            if(entry->index_to_workdir != nullptr)
-            {
-                delta = entry->index_to_workdir;
-            }
-           
-            return (delta != nullptr && delta->new_file.path != nullptr && (file == delta->new_file.path));
-        });
-
-        if(entry_it != entries.end())
-        {
-            const auto* entry = *entry_it;
-            result.status = gitxx::FlagField<gitxx::FileStatus>(entry->status);
-            result.head_to_index = DiffDeltaFromGit2(entry->head_to_index);
-            result.index_to_workdir = DiffDeltaFromGit2(entry->index_to_workdir);
-            return result;
+  auto entry_it =
+      std::ranges::find_if(entries, [&file](const git_status_entry *entry) {
+        const git_diff_delta *delta = nullptr;
+        if (entry->head_to_index != nullptr) {
+          delta = entry->head_to_index;
         }
+        if (entry->index_to_workdir != nullptr) {
+          delta = entry->index_to_workdir;
+        }
+
+        return (delta != nullptr && delta->new_file.path != nullptr &&
+                (file == delta->new_file.path));
+      });
+
+  if (entry_it != entries.end()) {
+    const auto *entry = *entry_it;
+    result.status = gitxx::FlagField<gitxx::FileStatus>(entry->status);
+    result.head_to_index = DiffDeltaFromGit2(entry->head_to_index);
+    result.index_to_workdir = DiffDeltaFromGit2(entry->index_to_workdir);
+    return result;
+  }
 
   return result;
 }
--- src/include/converters.hpp
+++ src/include/converters.hpp
@@ -1,6 +1,11 @@
+#include <git2/status.h>
+
 #include <cstdlib>
 #include <gitxx/diff_delta.hpp>
 #include <gitxx/error.hpp>
+#include <gitxx/status.hpp>
+
+#include "gitxx/status_options.hpp"
 
 namespace gitxx::internal {
 
@@ -13,9 +18,29 @@ struct conversion_traits<GitErrc, int> {
   static GitErrc from_c(const int& error) {
     return static_cast<GitErrc>(std::abs(error));
   }
-  static int from_cpp(const GitErrc& error){
-      return static_cast<int>(error);
+
+  static int from_cpp(const GitErrc& error) { return static_cast<int>(error); }
+};
+
+template <>
+struct conversion_traits<git_status_options, gitxx::StatusOptions> {
+  static std::optional<gitxx::StatusOptions> from_c(
+      const git_status_options* option) {
+      // do we ever need this?
+      static_cast<void>(option);
+      return std::nullopt;
+  }
+
+  static git_status_options from_cpp(const gitxx::StatusOptions& options) {
+    return {.version = 1U,
+            .show = static_cast<git_status_show_t>(options.show),
+            .flags = options.flags.value(),
+            .pathspec = {nullptr, 0U},
+            .baseline = nullptr,
+            .rename_threshold = options.rename_threshold};
   }
 };
+
+
 
 }  // namespace gitxx::internal
