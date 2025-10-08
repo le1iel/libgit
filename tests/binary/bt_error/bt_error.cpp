39eedfc86b8a88cb1835e8af60d2813f3f9cdabf
le1iel <conform2187@proton.me>
2025-10-08 07:12:47 +0100

    Redoing test to get 100% coverage

--- tests/binary/bt_error/bt_error.cpp
+++ tests/binary/bt_error/bt_error.cpp
@@ -2,13 +2,51 @@
 
 #include <gitxx/error.hpp>
 
-TEST(bt_error, message) {
-  std::error_code ec = GitErrc::eof;
-  std::cout << ec << std::endl;
-  std::cout << ec.message() << std::endl;
+
+class bt_error :public ::testing::TestWithParam<GitErrc> {};
+
+TEST_P(bt_error, message) {
+  std::error_code error_code = GetParam();
+  std::string message = error_code.message();
+  EXPECT_FALSE(message.empty());
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+INSTANTIATE_TEST_SUITE_P(
+        bt_error,
+        bt_error,
+        ::testing::Values(
+            GitErrc::not_found,
+            GitErrc::exists,
+            GitErrc::ambiguous,
+            GitErrc::bufs,
+            GitErrc::user,
+            GitErrc::bare_repo,
+            GitErrc::unborn_branch,
+            GitErrc::unmerged,
+            GitErrc::non_fast_forward,
+            GitErrc::invalid_spec,
+            GitErrc::conflict,
+            GitErrc::locked,
+            GitErrc::modified,
+            GitErrc::auth,
+            GitErrc::certificate,
+            GitErrc::applied,
+            GitErrc::peel,
+            GitErrc::eof,
+            GitErrc::invalid,
+            GitErrc::uncommitted,
+            GitErrc::directory,
+            GitErrc::merge_conflict,
+            GitErrc::passthrough,
+            GitErrc::iterover,
+            GitErrc::retry,
+            GitErrc::mismatch,
+            GitErrc::index_dirty,
+            GitErrc::apply_fail,
+            GitErrc::owner,
+            GitErrc::timeout,
+            GitErrc::unchanged,
+            GitErrc::not_supported,
+            GitErrc::readonly,
+            static_cast<GitErrc>(1)
+));
